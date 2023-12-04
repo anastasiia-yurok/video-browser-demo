96e510a80235f5abfdc5d958d6e871d7b2398d87
Anastasiia Yurok <115250654+anastasiia-yurok@users.noreply.github.com>
2023-12-04 09:40:16 +0200

    Project setup

---
+++ video-browser-demo/video-browser-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video-browser-demo
+//
+//  Created by Anastasia Yurok on 04.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
